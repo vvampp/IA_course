54153e6d4136f8297aac92535f2b3ec2303d27aa
vvampp <bamportela.n@gmail.com>
2025-11-06 21:38:25 -0600

    CMakefile

--- Pract05/minimum_distance_classifier/python/classifier.py
+++ Pract05/minimum_distance_classifier/python/classifier.py
@@ -2,42 +2,26 @@ import numpy as np
 from . import mdc_core
 
 class MinimumDistanceClassifier:
-    def __init__(self, use_cuda=True):
-        #  instanciate C++ object
+    def __init__(self, use_cuda=False):
         self._model = mdc_core.MinimumDistanceClassifier(use_cuda)
-
+    
     def fit(self, X, y):
-        # numpy to lists if necesarry
-        if isinstance(X, np.ndarray):
-            X_list = X.astype(np.float32).tolist()
-        else:
-            X_list = X
-            
-        if isinstance(y, np.ndarray):
-            y_list = y.astype(np.int32).tolist()
-        else:
-            y_list = y
-        
-        # call C++ method
-        self._model.fit(X_list, y_list)
+        X = np.asarray(X, dtype=np.float32)
+        y = np.asarray(y, dtype=np.int32)
+        self._model.fit(X, y)
         return self
-
+    
     def predict(self, X):
-        if isinstance(X, np.ndarray):
-            X_list = X.astype(np.float32).tolist()
-        else:
-            X_list = X
-            
-        # call to the C++ method, returns python list
-        preds_list = self._model.predict(X_list)
-        
-        # result to numpy array
-        return np.array(preds_list, dtype=np.int32)
-
+        X = np.asarray(X, dtype=np.float32)
+        return self._model.predict(X)
+    
     def get_centroids(self):
-        
-        # call to C++ method; returns a list of lists
-        centroids_list = self._model.get_centroids()
-        
-        # to numpy array
-        return np.array(centroids_list, dtype=np.float32)
+        return self._model.get_centroids()
+    
+    @property
+    def is_using_cuda(self):
+        return self._model.is_using_cuda()
+    
+    @property
+    def is_fitted(self):
+        return self._model.is_fitted()
--- Pract05/minimum_distance_classifier/src/python_bindings.cpp
+++ Pract05/minimum_distance_classifier/src/python_bindings.cpp
@@ -1,27 +1,56 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "classifier.cpp"
+#include "classifier.hpp"
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(mdc_core, m){
-  m.doc() = "Python bindings for Minimum Distance Classifier (C++/CUDA)";
-
-  py::class_<mdc::MinimumDistanceClassifier>(m, "MinimumDistanceClassifier")
-    // constructor call
-    .def(py::init<bool>(), py::arg("use_cuda") = true)
-
-    // fit method call
-    .def("fit", &mdc::MinimumDistanceClassifier::fit,
-         "Fit the model using training data X  and labels y",
-         py::arg("X"), py::arg("y"))
-
-    // predict method call
-    .def("predict", &mdc::MinimumDistanceClassifier::predict,
-         "Predict class labels for samples in X (float)",
-         py::arg("X"))
-
-    // get centroids metho call
-    .def("get_centroids", &mdc::MinimumDistanceClassifier::get_centroids,
-         "Get the computed centroids after fitting");
+PYBIND11_MODULE(mdc_core, m) {
+    m.doc() = "Minimum Distance Classifier with C++/CUDA backend";
+    
+    py::class_<mdc::MinimumDistanceClassifier>(m, "MinimumDistanceClassifier")
+        .def(py::init<bool>(), 
+             py::arg("use_cuda") = false,
+             "Constructor\n\n"
+             "Parameters:\n"
+             "  use_cuda (bool): Enable CUDA acceleration")
+        
+        .def("fit", 
+             &mdc::MinimumDistanceClassifier::fit,
+             py::arg("X"), 
+             py::arg("y"),
+             "Train the classifier\n\n"
+             "Parameters:\n"
+             "  X: list of lists, shape (n_samples, n_features)\n"
+             "  y: list of ints, shape (n_samples,)")
+        
+        .def("predict", 
+             &mdc::MinimumDistanceClassifier::predict,
+             py::arg("X"),
+             "Predict class labels\n\n"
+             "Parameters:\n"
+             "  X: list of lists, shape (n_samples, n_features)\n"
+             "Returns:\n"
+             "  list of ints, shape (n_samples,)")
+        
+        .def("get_centroids", 
+             &mdc::MinimumDistanceClassifier::get_centroids,
+             "Get computed centroids\n\n"
+             "Returns:\n"
+             "  list of lists, shape (n_classes, n_features)")
+        
+        .def("is_using_cuda", 
+             &mdc::MinimumDistanceClassifier::is_using_cuda,
+             "Check if CUDA is being used")
+        
+        .def("is_fitted", 
+             &mdc::MinimumDistanceClassifier::is_fitted,
+             "Check if model has been trained")
+        
+        .def("get_n_classes", 
+             &mdc::MinimumDistanceClassifier::get_n_classes,
+             "Get number of classes")
+        
+        .def("get_n_features", 
+             &mdc::MinimumDistanceClassifier::get_n_features,
+             "Get number of features");
 }
