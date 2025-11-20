9c0feafc7fec388670ebd1751af9043ae60c03b5
vvampp <bamportela.n@gmail.com>
2025-11-20 17:41:05 -0600

    Pract06

---
+++ Pract05/minimum_distance_classifier/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import os
+import subprocess
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        
+        cmake_args = [
+            f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
+            f'-DPYTHON_EXECUTABLE={sys.executable}',
+            '-DUSE_CUDA=ON',
+            '-DBUILD_TESTS=OFF'
+        ]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        cmake_args += [f'-DCMAKE_BUILD_TYPE={cfg}']
+        build_args += ['--', '-j4']
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+setup(
+    name='mdc-classifier',
+    version='0.1.0',
+    author='Tu Nombre',
+    description='Minimum Distance Classifier with CUDA acceleration',
+    long_description='',
+    packages=['python'],
+    ext_modules=[CMakeExtension('mdc_core')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+    python_requires='>=3.8',
+)
